6b270dfc4dfec679bc8b4687163034f6ca770f39
Nischal Niroula <officialnischalniroula@gmail.com>
2024-04-07 01:33:49 +1100

    Initial Commit

---
+++ Groot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Groot
+//
+//  Created by Nischal Niroula on 7/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Groot/GrootApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrootApp.swift
+//  Groot
+//
+//  Created by Nischal Niroula on 7/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct GrootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
